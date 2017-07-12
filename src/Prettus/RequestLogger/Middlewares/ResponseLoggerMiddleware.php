5f93fdd85270da04507ccfaff2ca34340b5e9bce
Валиуллина Альбина <albi.valiullina@yandex.ru>
2017-07-12 15:07:38 +0300

    error modif

--- src/Prettus/RequestLogger/Middlewares/ResponseLoggerMiddleware.php
+++ src/Prettus/RequestLogger/Middlewares/ResponseLoggerMiddleware.php
@@ -38,7 +38,35 @@ class ResponseLoggerMiddleware
       }
       $s = number_format($this->end-$_SERVER['REQUEST_TIME_FLOAT'], 3);
       $s .=','. $request->fullUrl().",".(string) http_response_code()."\n";
-      $file = fopen($path.DIRECTORY_SEPARATOR."response-".date('Y-m-d'), "a");
-      fwrite($file, $s);
+      $file = fopen($path.DIRECTORY_SEPARATOR."response-".dat
+
+    use DispatchesJobs;
+    public function handle($request, Closure $next)
+     {
+        $this->start = microtime(true);
+
+        return $next($request);
+    }
+
+    public function terminate($request, $response)
+    {
+        $this->end = microtime(true);
+
+        $this->log($request);
+    }
+
+  }
+
+  protected function log($request)
+  {
+      $path = storage_path("framework".DIRECTORY_SEPARATOR."temp");
+
+        if( !file_exists($path)){
+            mkdir($path, 0777, true);
+        }
+        $s = 'Duration,'.number_format($this->end-$_SERVER['REQUEST_TIME_FLOAT'], 5);
+        $s = $s.',URL,'. $request->fullUrl()."\n";
+        $file = fopen($path.DIRECTORY_SEPARATOR."response-".date('Y-m-d'), "a");
+        fwrite($file, $s);
   }
 }
